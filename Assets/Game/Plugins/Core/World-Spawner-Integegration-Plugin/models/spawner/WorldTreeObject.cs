2006dd909de5ddb7721b0f5af1dfbbb803ce8bff
Nick Tarsillo <nicktarsillo@Nicks-MacBook-Pro.local>
2016-07-18 10:57:18 -0400

    Integrated world and spawner systems

---
+++ Assets/Game/Plugins/Core/World-Spawner-Integegration-Plugin/models/spawner/WorldTreeObject.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldTreeObject : TreeSpawnObject {
+	#region implemented abstract members of TreeSpawnObject
+
+	public override void OnDespawn ()
+	{
+		WorldObjectCache.Remove (GetComponent<WorldObject>().id);
+	}
+
+	public override void OnSpawn (Transform spawn)
+	{
+		string id = UniqueIDGenerator.GetUniqueID ();
+		spawn.GetComponent<WorldObject> ().id = id;
+		WorldObjectCache.Add (id, spawn.GetComponent<WorldObject> ());
+	}
+
+	#endregion
+
+
+}
